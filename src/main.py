314178544ce3baf895ffd52f79aec0bb49d8dbf4
quy pham <phamquycntta@gmail.com>
2021-08-21 01:07:53 +0700

    edit-title

--- src/main.py
+++ src/main.py
@@ -13,4 +13,4 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
