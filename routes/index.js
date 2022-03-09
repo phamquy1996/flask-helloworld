50f2c1a726aa3a303560bef696b51c647d009d1e
thainv <thainv@its-global.vn>
2022-03-09 17:48:18 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Docker NodeJS', name: 'James' });
+  res.render('index', { title: 'Docker NodeJS Pro 9x 123', name: 'James' });
 });
 
 module.exports = router;
